dfd2f30c6e178d1bb369b146190d34bb1bcaceff
Manish Luste <59191348+manish24-tech@users.noreply.github.com>
2023-07-20 13:42:55 +0530

    Update RestTemplateServiceConfiguration.java
    updated code with generics

--- Springboot_Configuration/RestTemplateServiceConfiguration.java
+++ Springboot_Configuration/RestTemplateServiceConfiguration.java
@@ -1,4 +1,3 @@
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
@@ -15,30 +14,27 @@ public class RestTemplateConfig {
         return new RestTemplate();
     }
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    // Utility method for HTTP POST with HttpHeaders
-    public <T> ResponseEntity<T> post(String url, T requestBody, Class<T> responseType, HttpHeaders headers) {
+    // Utility method for HTTP POST with HttpHeaders and generic request and response types
+    public <T, R> ResponseEntity<R> post(String url, T requestBody, HttpHeaders headers, Class<R> responseType) {
         HttpEntity<T> requestEntity = new HttpEntity<>(requestBody, headers);
         return restTemplate.exchange(url, HttpMethod.POST, requestEntity, responseType);
     }
 
-    // Utility method for HTTP GET with HttpHeaders
-    public <T> ResponseEntity<T> get(String url, Class<T> responseType, HttpHeaders headers) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(headers);
+    // Utility method for HTTP GET with HttpHeaders and generic response type
+    public <R> ResponseEntity<R> get(String url, HttpHeaders headers, Class<R> responseType) {
+        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
         return restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
     }
 
-    // Utility method for HTTP PUT with HttpHeaders
-    public <T> ResponseEntity<T> put(String url, T requestBody, Class<T> responseType, HttpHeaders headers) {
+    // Utility method for HTTP PUT with HttpHeaders and generic request and response types
+    public <T, R> ResponseEntity<R> put(String url, T requestBody, HttpHeaders headers, Class<R> responseType) {
         HttpEntity<T> requestEntity = new HttpEntity<>(requestBody, headers);
         return restTemplate.exchange(url, HttpMethod.PUT, requestEntity, responseType);
     }
 
-    // Utility method for HTTP DELETE with HttpHeaders
-    public <T> ResponseEntity<T> delete(String url, Class<T> responseType, HttpHeaders headers) {
-        HttpEntity<T> requestEntity = new HttpEntity<>(headers);
+    // Utility method for HTTP DELETE with HttpHeaders and generic response type
+    public <R> ResponseEntity<R> delete(String url, HttpHeaders headers, Class<R> responseType) {
+        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
         return restTemplate.exchange(url, HttpMethod.DELETE, requestEntity, responseType);
     }
 }
